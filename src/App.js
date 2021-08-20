81afb9268cf4a6a0ce940b00b08b57635a4b378a
Alissonrf <alisonreinaldof21@gmail.com>
2021-08-20 16:42:02 -0300

    Props e component 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React from 'react';
 
-const BemVindo = (props) => {
-    return( 
+const Equipe = (props) => {
+    return(
         <div>
-            <h2>Bem Vindo(a) {props.nome} !</h2>
-            <h2>Tenho {props.idade} anos</h2>
+            <Sobre nome = {props.nome}
+                cargo = {props.cargo}
+                idade = {props.idade}/>
+            <Social fb = {props.facebook}/>
+                <hr/>
         </div>
-    );
+    )
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá! Sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+        </div>
+    )
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <a href = {props.fb}>Facebook</a>
+            <a>Linkedin</a>
+            <a>Youtube</a>
+        </div>
+    )
 }
 
 function App(){
     return(
         <div>
-            Olá Mundo!
-            <BemVindo nome = "Alisson" idade = "18" />
-            <BemVindo nome = "Julia" idade = "19" />
-            <h1>Curso React</h1>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome = "Alisson" cargo = "Programador Trainee" idade = "18" 
+                facebook = "https://google.com"/>
+
+            <Equipe nome = "Ariadne" cargo = "Designer" idade = "20"
+                facebook = "https://google.com" />
+
+            <Equipe nome = "Gustavo" cargo = "CEO" idade = "54"
+                facebook = "https://google.com" />
+            
         </div>
     );
 }
