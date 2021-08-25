9d934651ef7ac2be4be282fe6703043490bbcfee
Alissonrf <alisonreinaldof21@gmail.com>
2021-08-25 14:54:37 -0300

    Manipulando formularios pt2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
             nome: "",
             email: "",
             senha: "",
-            error: ''
+            error: ""
         }
         this.cadastrar = this.cadastrar.bind(this)
     }
@@ -16,35 +16,33 @@ class App extends Component{
     cadastrar(event){
         const {nome, email, senha} = this.state
 
-        if(nome != '' && email != '' && senha != ''){
+        if (nome !== '' && email !== '' && senha !== ''){
             alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
+        }else{
+            this.setState({error: "ops parece que está faltando algo!"})
         }
-        else{
-            this.setState({error: 'Ops! Parece que está faltando algo!'})
-        }
-        
 
+        // Quando clicar no botão de enviar a pagina nao apagara os dados no form
         event.preventDefault()
     }
 
     render(){
         return(
             <div>
-                <h1>Novo Usuario(a)</h1>
+                <h1>Novo Usuario</h1>
                 {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.cadastrar} >
+                <form onSubmit={this.cadastrar}>
                     <label>Nome:</label>
-                    <input type="text" value={this.state.nome}
-                         onChange={ (e) => {this.setState( {nome: e.target.value})}} /> <br/>
+                    <input type="text" value={this.state.nome} 
+                            onChange={(e) => this.setState({nome: e.target.value})} /> <br/>
 
                     <label>Email:</label>
                     <input type="email" value={this.state.email}
-                         onChange={ (e) => {this.setState( {email: e.target.value})}} /> <br/>
-                    
+                            onChange={(e) => this.setState({email: e.target.value})} /> <br/>
+
                     <label>Senha:</label>
                     <input type="password" value={this.state.senha}
-                         onChange={ (e) => {this.setState( {senha: e.target.value})}} /> <br/>
-                    
+                            onChange={(e) => this.setState({senha: e.target.value})} /> <br/>
                     <button type="submit">Enviar</button>
                 </form>
             </div>
